8e2c959dea1b4f8347898ee79042aaac5c837f57
Garavirod <rodrigogarciaavila26@gmail.com>
2019-03-14 21:25:35 -0600

    Practica 1
    Socket de flujo

---
+++ Practica1/src/Client/Client.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import static java.lang.Thread.sleep;
+import java.net.Socket;
+import javax.swing.JFileChooser;
+import javafx.stage.FileChooser;
+/**
+ *
+ * @author rodrigo
+ */
+public class Client {
+    public static void main(String[] args) {
+        ClienteArchivo();
+    }
+
+    public static void ClienteArchivo() {
+        try {
+            /* PREPARAMOS LOS DATOS DE LA CONEXION */
+            BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Escriba la direccion del servidor: ");
+            String host = teclado.readLine();
+            System.out.println("Escriba el puerto: ");
+            int port = Integer.parseInt(teclado.readLine());
+
+            /* SOCKET DEL LADO DEL CLIENTE */
+            Socket socketCliente = new Socket(host, port);
+
+            /* ACTIVAMOS MULTIPLE SELECCION */
+            JFileChooser jf = new JFileChooser();
+            jf.setMultiSelectionEnabled(true);
+
+            /* MOSTRAMOS EL FILECHOOSER */
+            int r = jf.showOpenDialog(null);
+
+            if (r == JFileChooser.APPROVE_OPTION) {
+
+                File[] f = jf.getSelectedFiles();
+
+                /* ENVIAMOS CANTIDAD DE ARCHIVOS */
+                /* OBTENEMOS EL CANAL DE SALIDA DE TIPO PRINT */
+                PrintWriter out = new PrintWriter(new OutputStreamWriter(socketCliente.getOutputStream()));
+                System.out.println("Enviando cantidad de archivos...");
+                out.println(f.length);
+                out.flush();
+
+                for (int i = 0; i < f.length; i++) {
+
+                    /* OBTENEMOS DATOS DE CADA ARCHIVO */
+                    String archivo = f[i].getAbsolutePath();
+                    String nombre = f[i].getName();
+                    long tam = f[i].length();
+
+                    /* OBTENEMOS EL CANAL DE ENTRADA DE TIPO DATA */
+                    DataInputStream entrada = new DataInputStream(new FileInputStream(archivo));
+
+                    /* OBTENEMOS EL CANAL DE SALIDA DE TIPO DATA */
+                    DataOutputStream salida = new DataOutputStream(socketCliente.getOutputStream());
+
+                    /* ENVIAMOS NOMBRE Y TAM */
+                    salida.writeUTF(nombre);
+                    salida.flush();
+                    salida.writeLong(tam);
+                    salida.flush();
+
+                    /* SECCION PARA EL ENVIO DEL ARCHIVO */
+                    byte[] b = new byte[1024];
+                    long enviado = 0;
+                    int porcentaje = 0, n = 0;
+
+                    /* CALCULAMOS EL PORCENTAJE */
+                    while (enviado < tam) {
+                        n = entrada.read(b);
+                        salida.write(b);
+                        salida.flush();
+                        enviado += n;
+                        porcentaje = (int) (enviado * 100 / tam);                        
+                        System.out.println("Enviado: " + porcentaje + "%\r");
+                    }
+
+                    /* CONCLUIMOS CON EL ARCHVIO i */
+                    System.out.println("Archivo " + f[i].getName() + " enviado.");
+
+                    /* SOLO CERRAMOS CUANDO SE ENVIEN TODOS */
+                    if (i == f.length - 1) {
+                        salida.close();
+                        entrada.close();
+                    }
+                }
+                out.close();
+                socketCliente.close();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Practica1/src/Server/Server.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package Server;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author rodrigo
+ */
+public class Server {
+    public static void main(String[] args) {
+        ServidorArchivo();
+    }
+
+    public static void ServidorArchivo() {
+        try {
+            /* PUERTO EN EL QUE ESCUCHA PETICIONES */
+            ServerSocket socketServidor = new ServerSocket(9002);
+            System.out.println("Esperando cliente...");
+
+            while (true) {
+                /* HASTA QUE RECIBA ALGUNA PETICION DEL CLIENTE */
+                Socket socketCliente = socketServidor.accept();
+                System.out.println(
+                        "Conexion establecida desde " + socketCliente.getInetAddress() + ":" + socketCliente.getPort());
+
+                /* ESTABLECEMOS EL CANAL DE ENTRADA DE TIPO BUFFERED */
+                BufferedReader in = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+                int cantidad = Integer.parseInt(in.readLine());
+                System.out.print("\nCantidad de archivos que se recibien: ");
+                System.out.print(cantidad + "\n");
+
+                for (int i = 0; i < cantidad; i++) {
+
+                    /* ESTABLECEMOS EL CANAL DE ENTRADA DE TIPO DATA */
+                    DataInputStream entrada = new DataInputStream(socketCliente.getInputStream());
+                    byte[] b = new byte[1024];
+                    String nombre = entrada.readUTF();
+                    System.out.println("Recibiendo el archivo: " + nombre);
+                    long tam = entrada.readLong();
+
+                    /* ESTABLECEMOS EL CANAL DE SALIDA DE TIPO DATA */
+                    DataOutputStream salida = new DataOutputStream(new FileOutputStream(nombre));
+
+                    /* SECCION PARA RECIBIR EL ARCHIVO */
+                    long recibido = 0;
+                    int n = 0, porcentaje = 0;
+
+                    while (recibido < tam) {
+                        n = entrada.read(b);
+                        salida.write(b, 0, n);
+                        salida.flush();
+
+                        /* PORCENTAJE */
+                        recibido += n;
+                        porcentaje = (int) (recibido * 100 / tam);
+                        System.out.println("Recibido: " + porcentaje + "%\r");
+                    }
+
+                    /* CONCLUIMOS CON EL ARCHVIO i */
+                    System.out.println("Archivo " + nombre + " recibido.");
+
+                    /* SOLO CERRAMOS CUANDO SE RECIBAN TODOS */
+                    if (i == cantidad - 1) {
+                        salida.close();
+                        entrada.close();
+                    }
+                }
+                in.close();
+                socketCliente.close();
+            }
+        } catch (
+
+        Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
